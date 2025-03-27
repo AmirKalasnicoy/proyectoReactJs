8eadaffaa0082264deea7e10522f5652ddd41dbd
Amir kalasnicoy <amirkalasnicoy1812@gmail.com>
2025-03-27 17:26:06 -0300

    agregue una img a una de las fundas y mejore el loader visualmente

--- react-proyect/src/components/Loader/Loader.jsx
+++ react-proyect/src/components/Loader/Loader.jsx
@@ -2,8 +2,10 @@ import './Loader.css';
 
 function Loader() {
     return (
+      <div className="loader-container">
         <span className="loader"></span>
+      </div>
     );
-};
+  }
 
 export default Loader;
