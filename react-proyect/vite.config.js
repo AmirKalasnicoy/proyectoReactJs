8006e1c9969b5b688b37de4d8071bbd12263edd9
Amir kalasnicoy <amirkalasnicoy1812@gmail.com>
2025-05-02 19:45:19 -0300

    testeando

--- react-proyect/vite.config.js
+++ react-proyect/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  // Esto se ejecuta después del build para copiar el archivo
+  closeBundle() {
+    copyFileSync('public/_redirects', 'dist/_redirects');
+  }
 })
